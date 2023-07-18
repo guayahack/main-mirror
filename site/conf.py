6f73339a69790f022e4e590fd68512b8e62907be
Jayson Salazar Rodriguez <mail@jdsalaro.com>
2023-07-18 23:35:30 +0200

    modified:   conf.py

--- site/conf.py
+++ site/conf.py
@@ -35,6 +35,7 @@ language = "es"
 
 html_title = "main@GuayaHack"
 html_logo = "_static/logo.png"
+html_favicon = "_static/logo.png"
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 
