dbee31095a546c22e50c8c2fc8a5af14d7bedfe9
Bliss <mmoralesx86@gmail.com>
2023-11-14 19:03:56 -0500

    add extension to conf.py

--- site/conf.py
+++ site/conf.py
@@ -23,6 +23,7 @@ extensions = [
     "sphinxext.rediraffe",
     "sphinxext.opengraph",
     "sphinx.ext.intersphinx",
+    "sphinx_new_tab_link",
     # "sphinx.ext.autosectionlabel", #not needed, we use myst_parser's implicit anchors
 ]
 
